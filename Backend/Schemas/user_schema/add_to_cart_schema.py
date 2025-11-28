511b88a24eb8af510dbff13b342ccb27816ccaa1
tamgau97 <tam.gau9788.k@gmail.com>
2025-11-28 10:21:08 +0530

    update in add_to_cart_schema.py

--- Backend/Schemas/user_schema/add_to_cart_schema.py
+++ Backend/Schemas/user_schema/add_to_cart_schema.py
@@ -2,7 +2,6 @@ from marshmallow import Schema, fields
 
 
 class AddToCartSchema(Schema):
-    email = fields.String(required=True)
     food_category = fields.String(required=True)
     food_name = fields.String(required=True)
     image_url = fields.Raw(required=True, metadata={'type': 'string', 'format': 'uri'})
