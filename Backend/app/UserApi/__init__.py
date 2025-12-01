9daa68e5621fe84663b62d916ae5a607d498bd07
tamgau97 <tam.gau9788.k@gmail.com>
2025-12-01 11:40:26 +0530

    update in backend folder

--- Backend/app/UserApi/__init__.py
+++ Backend/app/UserApi/__init__.py
@@ -1,13 +1,13 @@
-from add_to_cart import blp as UserAddItemBlueprint
-from fetch_add_to_cart_item import blp as UserFetchAllItemBlueprint
-from fetch_add_to_cart_item import blp as UserFetchItemBlueprint
-from get_all_ordered import blp as UserAllOrderedFood
-from get_food_data import blp as GetFoodItemBlueprint
-from update_profile import blp as ProfileUpdateBlueprint
-from user_food_order import blp as UserOrderFoodBlueprint
-from user_login import blp as UserLoginBlueprint
-from user_profile import blp as UserProfileBlueprint
-from user_register import blp as UserRegistrationBlueprint
+from .add_to_cart import blp as UserAddItemBlueprint
+from .fetch_add_to_cart_item import blp as UserFetchAllItemBlueprint
+from .fetch_add_to_cart_item import blp as UserFetchItemBlueprint
+from .get_all_ordered import blp as UserAllOrderedFood
+from .get_food_data import blp as GetFoodItemBlueprint
+from .update_profile import blp as ProfileUpdateBlueprint
+from .user_food_order import blp as UserOrderFoodBlueprint
+from .user_login import blp as UserLoginBlueprint
+from .user_profile import blp as UserProfileBlueprint
+from .user_register import blp as UserRegistrationBlueprint
 
 
 def register_user_blueprint(api):
--- Backend/app/adminApi/__init__.py
+++ Backend/app/adminApi/__init__.py
@@ -1,14 +1,14 @@
-from admin_delete_food import blp as AdminDeleteFoodItemBlueprint
-from admin_edit_food_data import blp as AdminEditFoodDataBlueprint
-from admin_login import blp as AdminLoginBlueprint
-from admin_logout import blp as AdminLogoutBlueprint
-from delete_staff import blp as AdminDeleteStaffBlueprint
-from food_data import blp as AdminInsertFoodDataBlueprint
-from get_all_order_data import blp as AdminGetAllOrderBlueprint
-from get_all_staff import blp as AdminGetAllStaffData
-from get_food import blp as AdminGetAllFoodBlueprint
-from staff_registeration import blp as AdminStaffRegistrationBlueprint
-from update_staff_data import blp as AdminUpdateStaffBlueprint
+from .admin_delete_food import blp as AdminDeleteFoodItemBlueprint
+from .admin_edit_food_data import blp as AdminEditFoodDataBlueprint
+from .admin_login import blp as AdminLoginBlueprint
+from .admin_logout import blp as AdminLogoutBlueprint
+from .delete_staff import blp as AdminDeleteStaffBlueprint
+from .food_data import blp as AdminInsertFoodDataBlueprint
+from .get_all_order_data import blp as AdminGetAllOrderBlueprint
+from .get_all_staff import blp as AdminGetAllStaffData
+from .get_food import blp as AdminGetAllFoodBlueprint
+from .staff_registeration import blp as AdminStaffRegistrationBlueprint
+from .update_staff_data import blp as AdminUpdateStaffBlueprint
 
 
 def register_admin_blueprint(api):
